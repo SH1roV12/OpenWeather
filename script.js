f1d0532502eea2753b96f4339d2ee8a6e2fa4902
Zakhar <yaharuott@localhost.localdomain>
2025-04-20 13:41:09 +0700

    fixed BEM and adaptive

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let input = document.getElementById("inputText");
-let btn = document.getElementById("getWeather")
-let weather = document.getElementById("result__temp")
-let cityInput = document.getElementById("result__city-name")
-let wind = document.getElementById("result__wind")
+let input = document.querySelector(".form__inputText");
+let btn = document.querySelector(".form__getWeather")
+let weather = document.querySelector(".result__temp")
+let cityInput = document.querySelector(".result__city-name")
+let wind = document.querySelector(".result__wind")
 btn.addEventListener("click", (e)=>{
     e.preventDefault();
     let city = input.value
@@ -10,16 +10,16 @@ btn.addEventListener("click", (e)=>{
     axios.post(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=00de4e9fb8c125725a689da8ef5cd393&units=metric`)
     .then(responce =>{
         console.log(responce)
-        weather.textContent = `Температура воздуха:  ${responce.data.main.temp}`
+        weather.textContent = `Температура воздуха:  ${responce.data.main.temp} °C`
         cityInput.textContent = city;
         wind.textContent = `Скорость ветра: ${responce.data.wind.speed} км/ч`;
-        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`
+        document.querySelector(".result__icon").src = `https://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`
     })
     .catch(err=>{
         console.error(err)
         cityInput.textContent = "Город не найден";
         weather.textContent = "";
         wind.textContent = "";
-        document.getElementById("icon").src = "";
+        document.querySelector(".result__icon").src = "";
     })
 })
